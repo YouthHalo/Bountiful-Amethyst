0420b825c3048ed91bf72d54ed68ed7876c9ac9c
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2025-08-13 12:49:30 -0400

    Initial Commit

---
+++ src/client/java/youthhalo/bountifulamethyst/BountifulAmethystClient.java
@@ -0,0 +1,10 @@
+package youthhalo.bountifulamethyst;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BountifulAmethystClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
